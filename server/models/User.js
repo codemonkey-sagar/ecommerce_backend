95f104bd5313b4fda4ab25c7ed1ab7df129c1291
Sagar Budhathoki <fmtsagar@gmail.com>
2023-10-01 00:30:31 +0545

    User model updated

--- server/models/User.js
+++ server/models/User.js
@@ -28,6 +28,48 @@ const userSchema = new Schema({
     type: String,
     trim: true,
   },
+  orders: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Order",
+  }],
+  wishLists: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "WishList",
+  }],
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  hasShippingAddress: {
+    type: Boolean,
+    default: false,
+  },
+  shippingAddress: {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    postalCode: {
+      type: String,
+    },
+    province: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    }
+  },
   createdAt: {
     type: Date,
     default: Date.now,
